8a04455e6f211af978566f40adef32157d9ddd0c
qfqvb9xz <zl2513@columbia.edu>
2017-05-08 20:08:19 -0400

    add background task scheduler

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import os
+import atexit
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -24,6 +27,18 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+def MLAPI():
+    with app.app_context():
+        User.ML()
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=MLAPI,
+    trigger=IntervalTrigger(seconds=3600),
+    replace_existing=True)
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     manager.run()
